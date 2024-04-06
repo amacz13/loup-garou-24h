6297ce727c32dba30b40e613e630ac0805d5f986
Roxane Le Tensorer <roxane.letensorer@gmail.com>
2024-04-06 14:59:04 +0200

    front : ajout de la conversation et des persos à tuer

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,13 +1,45 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+interface Player {
+  name: string;
+  image: string;
+}
 
 @Component({
+  standalone:true,
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  imports:[FormsModule, NgFor]
 })
 export class AppComponent {
-  title = 'front';
+  messages: string[] = [];
+  newMessage: string = '';
+
+  sendMessage() {
+    if (this.newMessage.trim() !== '') {
+      this.messages.push(`You: ${this.newMessage}`);
+      this.newMessage = '';
+    }
+  }
+
+  vote() {
+    // Logic to vote against a player
+    console.log("Voting against a player...");
+  }
+
+  players: Player[] = [
+    { name: 'Aphrodite', image: 'assets/characters/aphrodite.png' },
+    { name: 'Apollon', image: 'assets/characters/apollo.png' },
+    { name: 'Arès', image: 'assets/characters/ares.png' },
+    { name: 'Artémis', image: 'assets/characters/artemis.png' },
+    { name: 'Athéna', image: 'assets/characters/athena.png' },
+    { name: 'Hadès', image: 'assets/characters/hades.png' },
+    { name: 'Hermès', image: 'assets/characters/hermes.png' },
+    { name: 'Poséidon', image: 'assets/characters/poseidon.png' },
+    { name: 'Prométhée', image: 'assets/characters/prometheus.png' },
+  ];
 }
+
