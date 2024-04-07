89b643044ef8a605e256aaff29402c86d4800872
Roxane Le Tensorer <roxane.letensorer@gmail.com>
2024-04-07 05:03:25 +0200

    picto jour/nuit

--- front/src/app/components/header/header.component.ts
+++ front/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { GameStep } from '../../utils/game-steps.utils';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  @Input() gameStep: GameStep = GameStep.jour;
+  GameStep = GameStep
 
 }
